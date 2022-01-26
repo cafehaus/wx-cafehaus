4cba347d792cfeca0be01f0481ba8a9a256dd68f
ZHOU <510878689@qq.com>
2022-01-27 00:23:28 +0800

    完善风味轮图表，新增中文版本
    完善风味轮图表，新增中文版本，我的页面新增客服按钮

---
+++ pages/flavor-wheel/option.js
@@ -0,0 +1,833 @@
+const data = [
+  {
+    name: '花香',
+    itemStyle: {
+      color: '#da0d68'
+    },
+    children: [
+      {
+        name: '红茶',
+        value: 1,
+        itemStyle: {
+          color: '#975e6d'
+        }
+      },
+      {
+        name: '花香类',
+        itemStyle: {
+          color: '#e0719c'
+        },
+        children: [
+          {
+            name: '甘菊',
+            value: 1,
+            itemStyle: {
+              color: '#f99e1c'
+            }
+          },
+          {
+            name: '玫瑰',
+            value: 1,
+            itemStyle: {
+              color: '#ef5a78'
+            }
+          },
+          {
+            name: '茉莉',
+            value: 1,
+            itemStyle: {
+              color: '#f7f1bd'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '水果',
+    itemStyle: {
+      color: '#da1d23'
+    },
+    children: [
+      {
+        name: '莓果类',
+        itemStyle: {
+          color: '#dd4c51'
+        },
+        children: [
+          {
+            name: '黑莓',
+            value: 1,
+            itemStyle: {
+              color: '#3e0317'
+            }
+          },
+          {
+            name: '覆盆子',
+            value: 1,
+            itemStyle: {
+              color: '#e62969'
+            }
+          },
+          {
+            name: '蓝莓',
+            value: 1,
+            itemStyle: {
+              color: '#6569b0'
+            }
+          },
+          {
+            name: '草莓',
+            value: 1,
+            itemStyle: {
+              color: '#ef2d36'
+            }
+          }
+        ]
+      },
+      {
+        name: '干果类',
+        itemStyle: {
+          color: '#c94a44'
+        },
+        children: [
+          {
+            name: '葡萄干',
+            value: 1,
+            itemStyle: {
+              color: '#b53b54'
+            }
+          },
+          {
+            name: '西梅干',
+            value: 1,
+            itemStyle: {
+              color: '#a5446f'
+            }
+          }
+        ]
+      },
+      {
+        name: '其他水果',
+        itemStyle: {
+          color: '#dd4c51'
+        },
+        children: [
+          {
+            name: '椰子',
+            value: 1,
+            itemStyle: {
+              color: '#f2684b'
+            }
+          },
+          {
+            name: '樱桃',
+            value: 1,
+            itemStyle: {
+              color: '#e73451'
+            }
+          },
+          {
+            name: '石榴',
+            value: 1,
+            itemStyle: {
+              color: '#e65656'
+            }
+          },
+          {
+            name: '菠萝',
+            value: 1,
+            itemStyle: {
+              color: '#f89a1c'
+            }
+          },
+          {
+            name: '葡萄',
+            value: 1,
+            itemStyle: {
+              color: '#aeb92c'
+            }
+          },
+          {
+            name: '苹果',
+            value: 1,
+            itemStyle: {
+              color: '#4eb849'
+            }
+          },
+          {
+            name: '桃子',
+            value: 1,
+            itemStyle: {
+              color: '#f68a5c'
+            }
+          },
+          {
+            name: '梨',
+            value: 1,
+            itemStyle: {
+              color: '#baa635'
+            }
+          }
+        ]
+      },
+      {
+        name: '柑橘类',
+        itemStyle: {
+          color: '#f7a128'
+        },
+        children: [
+          {
+            name: '葡萄柚',
+            value: 1,
+            itemStyle: {
+              color: '#f26355'
+            }
+          },
+          {
+            name: '橙子',
+            value: 1,
+            itemStyle: {
+              color: '#e2631e'
+            }
+          },
+          {
+            name: '柠檬',
+            value: 1,
+            itemStyle: {
+              color: '#fde404'
+            }
+          },
+          {
+            name: '青柠',
+            value: 1,
+            itemStyle: {
+              color: '#7eb138'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '酸/\n发酵',
+    itemStyle: {
+      color: '#ebb40f'
+    },
+    children: [
+      {
+        name: '酸味',
+        itemStyle: {
+          color: '#e1c315'
+        },
+        children: [
+          {
+            name: '芳香酸',
+            value: 1,
+            itemStyle: {
+              color: '#9ea718'
+            }
+          },
+          {
+            name: '醋酸',
+            value: 1,
+            itemStyle: {
+              color: '#94a76f'
+            }
+          },
+          {
+            name: '丁酸',
+            value: 1,
+            itemStyle: {
+              color: '#d0b24f'
+            }
+          },
+          {
+            name: '异戊酸',
+            value: 1,
+            itemStyle: {
+              color: '#8eb646'
+            }
+          },
+          {
+            name: '柠檬酸',
+            value: 1,
+            itemStyle: {
+              color: '#faef07'
+            }
+          },
+          {
+            name: '苹果酸',
+            value: 1,
+            itemStyle: {
+              color: '#c1ba07'
+            }
+          }
+        ]
+      },
+      {
+        name: '酒精/\n发酵',
+        itemStyle: {
+          color: '#b09733'
+        },
+        children: [
+          {
+            name: '葡萄酒',
+            value: 1,
+            itemStyle: {
+              color: '#8f1c53'
+            }
+          },
+          {
+            name: '威士忌',
+            value: 1,
+            itemStyle: {
+              color: '#b34039'
+            }
+          },
+          {
+            name: '发酵',
+            value: 1,
+            itemStyle: {
+              color: '#ba9232'
+            }
+          },
+          {
+            name: '熟透',
+            value: 1,
+            itemStyle: {
+              color: '#8b6439'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '绿色/\n蔬菜',
+    itemStyle: {
+      color: '#187a2f'
+    },
+    children: [
+      {
+        name: '橄榄油',
+        value: 1,
+        itemStyle: {
+          color: '#a2b029'
+        }
+      },
+      {
+        name: '生的',
+        value: 1,
+        itemStyle: {
+          color: '#718933'
+        }
+      },
+      {
+        name: '绿色/\n蔬菜类',
+        itemStyle: {
+          color: '#3aa255'
+        },
+        children: [
+          {
+            name: '未成熟的',
+            value: 1,
+            itemStyle: {
+              color: '#a2bb2b'
+            }
+          },
+          {
+            name: '豌豆',
+            value: 1,
+            itemStyle: {
+              color: '#62aa3c'
+            }
+          },
+          {
+            name: '新鲜的',
+            value: 1,
+            itemStyle: {
+              color: '#03a653'
+            }
+          },
+          {
+            name: '墨绿色的',
+            value: 1,
+            itemStyle: {
+              color: '#038549'
+            }
+          },
+          {
+            name: '蔬菜',
+            value: 1,
+            itemStyle: {
+              color: '#28b44b'
+            }
+          },
+          {
+            name: '枯草',
+            value: 1,
+            itemStyle: {
+              color: '#a3a830'
+            }
+          },
+          {
+            name: '药草',
+            value: 1,
+            itemStyle: {
+              color: '#7ac141'
+            }
+          }
+        ]
+      },
+      {
+        name: '豆类',
+        value: 1,
+        itemStyle: {
+          color: '#5e9a80'
+        }
+      }
+    ]
+  },
+  {
+    name: '其他',
+    itemStyle: {
+      color: '#0aa3b5'
+    },
+    children: [
+      {
+        name: '纸浆/发霉',
+        itemStyle: {
+          color: '#9db2b7'
+        },
+        children: [
+          {
+            name: '陈腐味',
+            value: 1,
+            itemStyle: {
+              color: '#8b8c90'
+            }
+          },
+          {
+            name: '纸板味',
+            value: 1,
+            itemStyle: {
+              color: '#beb276'
+            }
+          },
+          {
+            name: '纸浆味',
+            value: 1,
+            itemStyle: {
+              color: '#fefef4'
+            }
+          },
+          {
+            name: '木头味',
+            value: 1,
+            itemStyle: {
+              color: '#744e03'
+            }
+          },
+          {
+            name: '发霉/潮湿',
+            value: 1,
+            itemStyle: {
+              color: '#a3a36f'
+            }
+          },
+          {
+            name: '发霉/灰尘',
+            value: 1,
+            itemStyle: {
+              color: '#c9b583'
+            }
+          },
+          {
+            name: '发霉/泥土',
+            value: 1,
+            itemStyle: {
+              color: '#978847'
+            }
+          },
+          {
+            name: '动物肉',
+            value: 1,
+            itemStyle: {
+              color: '#9d977f'
+            }
+          },
+          {
+            name: '肉汤',
+            value: 1,
+            itemStyle: {
+              color: '#cc7b6a'
+            }
+          },
+          {
+            name: '酚醛',
+            value: 1,
+            itemStyle: {
+              color: '#db646a'
+            }
+          }
+        ]
+      },
+      {
+        name: '化工味',
+        itemStyle: {
+          color: '#76c0cb'
+        },
+        children: [
+          {
+            name: '苦味',
+            value: 1,
+            itemStyle: {
+              color: '#80a89d'
+            }
+          },
+          {
+            name: '咸味',
+            value: 1,
+            itemStyle: {
+              color: '#def2fd'
+            }
+          },
+          {
+            name: '药味',
+            value: 1,
+            itemStyle: {
+              color: '#7a9bae'
+            }
+          },
+          {
+            name: '石油味',
+            value: 1,
+            itemStyle: {
+              color: '#039fb8'
+            }
+          },
+          {
+            name: '臭鼬味',
+            value: 1,
+            itemStyle: {
+              color: '#5e777b'
+            }
+          },
+          {
+            name: '橡皮味',
+            value: 1,
+            itemStyle: {
+              color: '#120c0c'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '烧焦',
+    itemStyle: {
+      color: '#c94930'
+    },
+    children: [
+      {
+        name: '烟斗丝味',
+        value: 1,
+        itemStyle: {
+          color: '#caa465'
+        }
+      },
+      {
+        name: '烟草味',
+        value: 1,
+        itemStyle: {
+          color: '#dfbd7e'
+        }
+      },
+      {
+        name: '烧焦',
+        itemStyle: {
+          color: '#be8663'
+        },
+        children: [
+          {
+            name: '苦味',
+            value: 1,
+            itemStyle: {
+              color: '#b9a449'
+            }
+          },
+          {
+            name: '灰烬味',
+            value: 1,
+            itemStyle: {
+              color: '#899893'
+            }
+          },
+          {
+            name: '烟熏味',
+            value: 1,
+            itemStyle: {
+              color: '#a1743b'
+            }
+          },
+          {
+            name: '烧黑, 烤焦',
+            value: 1,
+            itemStyle: {
+              color: '#894810'
+            }
+          }
+        ]
+      },
+      {
+        name: '谷物类',
+        itemStyle: {
+          color: '#ddaf61'
+        },
+        children: [
+          {
+            name: '谷物',
+            value: 1,
+            itemStyle: {
+              color: '#b7906f'
+            }
+          },
+          {
+            name: '麦芽',
+            value: 1,
+            itemStyle: {
+              color: '#eb9d5f'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '香辛味',
+    itemStyle: {
+      color: '#ad213e'
+    },
+    children: [
+      {
+        name: '刺鼻的',
+        value: 1,
+        itemStyle: {
+          color: '#794752'
+        }
+      },
+      {
+        name: '胡椒味',
+        value: 1,
+        itemStyle: {
+          color: '#cc3d41'
+        }
+      },
+      {
+        name: '棕色香料',
+        itemStyle: {
+          color: '#b14d57'
+        },
+        children: [
+          {
+            name: '茴香',
+            value: 1,
+            itemStyle: {
+              color: '#c78936'
+            }
+          },
+          {
+            name: '肉豆蔻',
+            value: 1,
+            itemStyle: {
+              color: '#8c292c'
+            }
+          },
+          {
+            name: '肉桂',
+            value: 1,
+            itemStyle: {
+              color: '#e5762e'
+            }
+          },
+          {
+            name: '丁香',
+            value: 1,
+            itemStyle: {
+              color: '#a16c5a'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '坚果/\n可可',
+    itemStyle: {
+      color: '#a87b64'
+    },
+    children: [
+      {
+        name: '坚果类',
+        itemStyle: {
+          color: '#c78869'
+        },
+        children: [
+          {
+            name: '花生',
+            value: 1,
+            itemStyle: {
+              color: '#d4ad12'
+            }
+          },
+          {
+            name: '榛果',
+            value: 1,
+            itemStyle: {
+              color: '#9d5433'
+            }
+          },
+          {
+            name: '杏仁',
+            value: 1,
+            itemStyle: {
+              color: '#c89f83'
+            }
+          }
+        ]
+      },
+      {
+        name: '可可类',
+        itemStyle: {
+          color: '#bb764c'
+        },
+        children: [
+          {
+            name: '巧克力',
+            value: 1,
+            itemStyle: {
+              color: '#692a19'
+            }
+          },
+          {
+            name: '黑巧克力',
+            value: 1,
+            itemStyle: {
+              color: '#470604'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '甜味',
+    itemStyle: {
+      color: '#e65832'
+    },
+    children: [
+      {
+        name: '红糖',
+        itemStyle: {
+          color: '#d45a59'
+        },
+        children: [
+          {
+            name: '糖浆',
+            value: 1,
+            itemStyle: {
+              color: '#310d0f'
+            }
+          },
+          {
+            name: '枫糖',
+            value: 1,
+            itemStyle: {
+              color: '#ae341f'
+            }
+          },
+          {
+            name: '焦糖',
+            value: 1,
+            itemStyle: {
+              color: '#d78823'
+            }
+          },
+          {
+            name: '蜂蜜',
+            value: 1,
+            itemStyle: {
+              color: '#da5c1f'
+            }
+          }
+        ]
+      },
+      {
+        name: '香草味',
+        value: 1,
+        itemStyle: {
+          color: '#f89a80'
+        }
+      },
+      {
+        name: '香草醛',
+        value: 1,
+        itemStyle: {
+          color: '#f37674'
+        }
+      },
+      {
+        name: '甜腻',
+        value: 1,
+        itemStyle: {
+          color: '#e75b68'
+        }
+      },
+      {
+        name: '芳香甜',
+        value: 1,
+        itemStyle: {
+          color: '#d0545f'
+        }
+      }
+    ]
+  }
+]
+
+const option = {
+  title: {
+    text: "SCAA 咖啡风味轮",
+    subtext: '世界咖啡研究感官词典',
+    textStyle: {
+      fontSize: 18,
+      align: 'center'
+    },
+    subtextStyle: {
+      align: 'center'
+    },
+    left: 'center',
+    // top: 10,
+  },
+  textStyle: {
+    fontSize: 6,
+  },
+  series: {
+    radius: ['15%', '80%'],
+    type: 'sunburst',
+    sort: null,
+    highlightPolicy: 'ancestor',
+    data: data,
+    label: {
+      rotate: 'radial'
+    },
+    levels: [],
+    itemStyle: {
+      color: '#ddd',
+      borderWidth: 2
+    }
+  }
+}
+
+module.exports = option
